ea26e384ae422bd7055883297cb373cd26d8fde2
man7788 <jerman1841@gmail.com>
2025-08-15 02:42:12 +0800

    create routes for index redirection

---
+++ routes/indexRouter.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+
+const indexController = require('../controllers/indexController');
+
+const indexRouter = Router();
+
+// Index Routes
+indexRouter.get('/', indexController.getIndex);
+
+// Log-in Routes
+indexRouter.get('/log-in', indexController.getLogin);
+
+// Main Routes
+indexRouter.get('/main', indexController.getMain);
+
+module.exports = indexRouter;
