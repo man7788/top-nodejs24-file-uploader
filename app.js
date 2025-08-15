22e7b60bf9009845c4c035fcf111923f5bde3f9c
man7788 <jerman1841@gmail.com>
2025-08-16 03:25:28 +0800

    create app use connect-flash

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('node:path');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const flash = require('connect-flash');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
 
 const indexRouter = require('./routes/indexRouter');
 
