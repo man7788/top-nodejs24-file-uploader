099df540f0696e433effc28e3bce9218a209f019
man7788 <jerman1841@gmail.com>
2025-08-16 02:32:34 +0800

    create read user queries

--- prisma/queries.js
+++ prisma/queries.js
@@ -1,11 +1,35 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// Create User
 exports.createUser = async (email, password) => {
   const user = await prisma.user.create({
     data: {
       email,
       password,
+    },
+  });
+  return user;
+};
+
+// Read User
+exports.readUserByEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+};
+
+exports.readUserById = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      email: true,
     },
   });
   return user;
