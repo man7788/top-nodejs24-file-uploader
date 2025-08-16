3206d59a8f475b61e3d08ed9f8a75ba2af11be21
man7788 <jerman1841@gmail.com>
2025-08-16 10:44:35 +0800

    create post uploader route

--- routes/uploaderRouter.js
+++ routes/uploaderRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const multer = require('multer');
 
 const uploaderController = require('../controllers/uploaderController');
 
 const uploaderRouter = Router();
 
-//Uploader Routes
+// Uploader Routes
 uploaderRouter.get('/', uploaderController.getUploader);
+uploaderRouter.post('/', uploaderController.postUploader);
 
 module.exports = uploaderRouter;
