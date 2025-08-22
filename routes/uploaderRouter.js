9bb5fa0cfa845b46420bca340a1109cf4496f99c
man7788 <jerman1841@gmail.com>
2025-08-22 08:01:10 +0800

    create post folder route

--- routes/uploaderRouter.js
+++ routes/uploaderRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 
 const uploaderController = require('../controllers/uploaderController');
 
-const uploaderRouter = Router({ mergeParams: true });
+const uploaderRouter = Router();
 
 // Uploader Routes
 uploaderRouter.get('/', uploaderController.getUploader);
@@ -10,5 +10,6 @@ uploaderRouter.post('/', uploaderController.postUploader);
 
 // Folder Routes
 uploaderRouter.get('/*folders', uploaderController.getFolder);
+uploaderRouter.post('/folder', uploaderController.postFolder);
 
 module.exports = uploaderRouter;
