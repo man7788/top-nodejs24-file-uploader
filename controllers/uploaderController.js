d1e581e4f8c7b52392a05fb5254db61a4e25fa8d
man7788 <jerman1841@gmail.com>
2025-08-22 08:01:22 +0800

    create post folder controller

--- controllers/uploaderController.js
+++ controllers/uploaderController.js
@@ -1,5 +1,16 @@
+const { body, validationResult } = require('express-validator');
 const db = require('../prisma/queries');
 const multer = require('multer');
+
+const nameErr = 'must be between 1 and 255 characters.';
+
+const validateFolder = [
+  body('newFolder')
+    .trim()
+    .isLength({ min: 1, max: 255 })
+    .withMessage(`Folder name ${nameErr}`)
+    .escape(),
+];
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -17,10 +28,18 @@ const upload = multer({ storage: storage });
 exports.getUploader = async (req, res) => {
   if (req.isAuthenticated()) {
     const root = await db.readFolder(res.locals.currentUser.id);
+    // Content flash from create folder
+    const errors = req.flash('errors');
+
     res.locals.folders = root.subFolders;
+    // Session property for create folder redirect
+    req.session.path = null;
+    req.session.superFolder = root.id;
+
     res.render('uploader', {
       title: 'Uploader',
       filePath: null,
+      errors,
     });
   } else {
     res.redirect('/log-in');
@@ -43,15 +62,17 @@ exports.getFolder = async (req, res) => {
     let pathString = '/';
     path.forEach((name, index) => {
       if (index === path.length - 1) {
-        return (pathString = pathString + name);
+        pathString = pathString + name;
+        return pathString;
       }
+
       pathString = pathString + name + '/';
     });
 
     let backPath = '/';
     path.forEach((name, index) => {
       if (path.length === 1) {
-        backPath = backPath.slice(0, -1);
+        backPath = '';
         return backPath;
       }
 
@@ -80,11 +101,19 @@ exports.getFolder = async (req, res) => {
     const traversePath = async (folder, index = 0) => {
       // Finished traversing, 1 index ahead of path array
       if (index > path.length - 1) {
+        // Content flash from create folder
+        const errors = req.flash('errors');
+
         res.locals.folders = folder.subFolders;
+        // Session property for create folder redirect
+        req.session.path = path;
+        req.session.superFolder = folder.id;
+
         return res.render('uploader', {
           title: 'Uploader',
           filePath: pathString,
           backPath,
+          errors,
         });
       }
 
@@ -112,3 +141,39 @@ exports.getFolder = async (req, res) => {
     res.redirect('/log-in');
   }
 };
+
+exports.postFolder = [
+  validateFolder,
+  async (req, res) => {
+    // Session property from get uploader or get folder
+    const path = req.session.path;
+    let pathString = '';
+
+    if (req.session.path) {
+      pathString = '/';
+      path.forEach((name, index) => {
+        if (index === path.length - 1) {
+          pathString = pathString + name;
+          return pathString;
+        }
+
+        pathString = pathString + name + '/';
+      });
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash('errors', errors.array());
+      return res.status(400).redirect(`/uploader${pathString}`);
+    }
+
+    await db.createFolder(
+      req.body.newFolder,
+      // Session property from get uploader or get folder
+      req.session.passport.user,
+      req.session.superFolder
+    );
+
+    res.redirect(`/uploader${pathString}`);
+  },
+];
