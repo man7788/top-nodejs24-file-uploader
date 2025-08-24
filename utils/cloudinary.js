a97532bf145564e09db14346fc932c2c46c3ac09
man7788 <jerman1841@gmail.com>
2025-08-25 02:41:29 +0800

    create cloudinary config

---
+++ utils/cloudinary.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+// Require the cloudinary library
+const cloudinary = require('cloudinary').v2;
+
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true,
+});
+
+exports.uploadImage = async (imagePath) => {
+  // Use the uploaded file's name as the asset's public ID and
+  // allow overwriting the asset with new versions
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
+
+  try {
+    // Upload the image
+    const result = await cloudinary.uploader.upload(imagePath, options);
+    console.log(result);
+    return result.url;
+  } catch (error) {
+    console.error(error);
+  }
+};
